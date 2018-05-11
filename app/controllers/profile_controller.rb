c5391ddf290505df3733d3ef1c3bad479955302c
JNelson <nelson.jimmy@gmail.com>
2018-05-11 05:58:49 -0600

    Added pinned repositories. Formatting not perfect yet

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,5 +2,6 @@ class ProfileController < ApplicationController
   def index
     session[:github_user] = params[:github_user]
     @user_info = UserInfo.new(session[:github_user], session[:token])
+    @github_repos = Repositories.new(session[:github_user], session[:token], '/repos')
   end
 end
